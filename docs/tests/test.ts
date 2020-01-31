94203a3b9cb7e889c10da92e2d7f5d49fa154853
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2020-01-31 19:01:55 +0100

    test: Add typescript test file

---
+++ docs/tests/test.ts
@@ -0,0 +1,11 @@
+// #Metrics: {"complexity": 3}
+
+// #LineComplexity: 3
+function f2(param: number): string {
+    while(true) {
+        while(true) {
+            console.log("Hello");
+        }
+    }
+    return param.toString();
+}
