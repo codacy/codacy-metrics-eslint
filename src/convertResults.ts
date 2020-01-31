ec1d6082aad682e8e50c6fd4bdd1fc22e625bd3c
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2020-01-31 18:10:23 +0100

    clean: Use reduce instead of filtering undefined values

--- src/convertResults.ts
+++ src/convertResults.ts
@@ -8,21 +8,19 @@ const ComplexityRegex = /(?:Arrow function|Function|Constructor|Method)\s*.*?\s*
 export function convertResults(report: CLIEngine.LintReport): CodacyResult[] {
   return flatMap(report.results, result => {
     const filename = result.filePath
-    const lineComplexities = result.messages.map(m => {
+    
+    const lineComplexities = result.messages.reduce<LineComplexity[]>((acc, m) => {
       const line = m.line
       const complexity = m.message.match(ComplexityRegex)
       if (complexity && complexity.length == 2) {
         const parsedComplexity = parseInt(complexity[1])
         if (parsedComplexity != NaN)
-          return new LineComplexity(line, parsedComplexity)
+          acc.push(new LineComplexity(line, parsedComplexity))
       }
-      return undefined
-    })
-    const filteredLineComplexities = lineComplexities.filter(
-      c => c
-    ) as LineComplexity[]
-    const complexity = Math.max(...filteredLineComplexities.map(lc => lc.value))
-    return new CodacyResult(filename, complexity, filteredLineComplexities)
+      return acc
+    }, [])
+    const complexity = Math.max(...lineComplexities.map(lc => lc.value))
+    return new CodacyResult(filename, complexity, lineComplexities)
   })
 }
 
